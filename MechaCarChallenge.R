adb7dfdae9a79a3546a1d7b8168c58f2613a300a
stereo-chemistry <remembertherodeo@gmail.com>
2022-02-14 17:34:01 -0600

    Pushing deliverable 2 statistic summaries

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,24 @@
 library("dplyr") #import dplyr package dependency
 
+#Deliverable 1
+
 mechacar_data = read.csv(file='MechaCar_mpg.csv', stringsAsFactors=FALSE) #import csv file as data frame
 
 mechacar_plt = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_data) #generate linear regression against mpg
 summary(mechacar_plt) #summarize it
 plot(mechacar_plt) #plots!
+
+#Deliverable 2
+
+suspensionCoil_data = read.csv(file='Suspension_Coil.csv', stringsAsFactors=FALSE)
+
+total_summary = summarise(suspensionCoil_data,
+                          Mean=mean(PSI, na.rm=TRUE),
+                          Median=median(PSI, na.rm=TRUE),
+                          Variance=var(PSI, na.rm=TRUE),
+                          SD=sd(PSI, na.rm=TRUE))
+
+lot_summary = suspensionCoil_data %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI, na.rm=TRUE),
+                                                                                Median=median(PSI, na.rm=TRUE),
+                                                                                Variance=var(PSI, na.rm=TRUE),
+                                                                                SD=sd(PSI, na.rm=TRUE))
